d13b510f822f09249da5120df9a5a2426a969809
Ashish ranjan <64465716+ashish-rgb@users.noreply.github.com>
2022-09-30 12:30:52 +0530

    Create Undergrad.java

---
+++ Undergrad.java
@@ -0,0 +1,13 @@
+package com.nissan.bean;
+
+public class Undergrad extends Student1{
+
+	public boolean isPassed(double grade) {
+		if(grade>70){
+		return true;
+		}
+		else
+		return false;
+		}
+	
+}
